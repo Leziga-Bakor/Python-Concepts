442393c5b43e2612b546d4f1963827ecadeabcfa
Leziga Bakor <85461869+leziga-bakor-epam@users.noreply.github.com>
2023-08-19 22:16:36 +0000

    python name mangling

--- name_mangling.py
+++ name_mangling.py
@@ -1,1 +1,23 @@
+class A:
+    __count = 0
 
+    def get_count(self):
+        return self.__count
+
+    def set_count(self, count):
+        self.__count = count
+
+
+def main():
+    
+    a = A()
+    print(a.get_count())
+
+    a.__count = 42
+    print(a.get_count())
+
+    print(a.__dict__)
+
+
+if __name__ == "__main__":
+    main()
